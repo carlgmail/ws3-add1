3596deb48ebc612c770043c7043cb7c59451c1e5
carlgmail <carl.santoso@gmail.com>
2016-11-30 15:14:05 +0800

    Create Custom.js
    Create first file to change

---
+++ custom.js
@@ -0,0 +1,26 @@
+Hi
+This Workshop is available for 19 Trainee
+
+The attenddee are :
+
+1 . Adhi Krisna Indra Prasetya
+2 . Choirul Anas
+3 . David Kristiawan
+4 . Dewa Putu Wijaya Kusuma
+5 . GB Agung Pradnya Andikaputra
+6 . Helmy Satria
+7 . Kasuma Yuda
+8 . Mohammad Fatkhan Fauzi
+9 . Muhamad Nizham
+10. Muhamad Ridwan
+11. Muhammad Erik Rahman Mulyana
+12. Muhammad Faza Mu’tashim
+13. Muslim Ilmiawan
+14. Rifan Muhammad Fauzi
+15. Robby Maulana
+16. Sandi Rachmad Buchori
+17. Sean Satya Henura
+18. Yohanes Ricky Andika Pradana
+19. Yordan Kristianto
+
+Please joinning the session so we can try at least.
